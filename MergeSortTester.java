e5185d1ba0636947539ec89cb00d15e20e78d5d4
Jenny Han <jhan6@stuy.edu>
2017-02-15 01:06:40 -0500

    More trials for each array, modified README

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -8,7 +8,7 @@
   <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
 
   Mean execution times for dataset of size n:
-  Batch size: 10
+  Batch size: 10000
   n=1       time: 
   n=10      time: 
   n=100     time: 
@@ -25,21 +25,23 @@ public class MergeSortTester
     //helper method for arrays
     public static void fill(int[] ray){
 	for(int i = 0; i < ray.length; i++){
-	    ray[i] = ray.length - i;
+	    ray[i] = (int) (Math.random() * 10000);
 	}
     }
 
-    public static void time(int[] ray){
+    public static long time(int[] ray){
+	long start = System.nanoTime();
+	MergeSort.sort(ray);
+	long end = System.nanoTime();
+	return end - start; 
+    }
+
+    public static void averagePrint (int[] arr) {
 	long counter = 0;
-	for (int i = 0; i < 10; i++){
-	    long start = System.nanoTime();
-	    MergeSort.sort(ray);
-	    long end = System.nanoTime();
-	    long taken = end - start;
-	    counter += taken;
-	    System.out.println("\tTrial " + (i + 1) + ": " + taken + " nanoseconds.");
+	for (int i = 0; i < 10000; i ++) {
+	    counter += (time (arr));
 	}
-	System.out.println("Average: " + (counter / 10) + " nanoseconds.");
+	System.out.println("Average: " + (counter / 10000) + " nanoseconds.");
     }
 
     /******************************
@@ -51,46 +53,48 @@ public class MergeSortTester
     public static void main( String[] args )
     {
 	//----------- Creating Arrays ----------
-	int[] descending1 = {0};
-	int[] descending10 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	int[] arr1 = new int [1];
+	int[] arr10 = new int[10]; 
 	
-	int[] descending100 = new int[100];
-	int[] descending1000 = new int[1000];
-	int[] descending10000 = new int[10000];
+	int[] arr100 = new int[100];
+	int[] arr1000 = new int[1000];
+	int[] arr10000 = new int[10000];
 
-	fill(descending100);
-	fill(descending1000);
-	fill(descending10000);
+	fill(arr1);
+	fill(arr10);
+	fill(arr100);
+	fill(arr1000);
+	fill(arr10000);
 
 	//--------------------------------------
 
         System.out.print( "Array in descending order:\n\n" );
 
         System.out.println( "n = 1:");
-        time(descending1);
+        averagePrint(arr1);
 
 	System.out.println();
 	
         System.out.println( "n = 10:");
-        time(descending10);
+        averagePrint(arr10);
 	
 
 	System.out.println();
 	
         System.out.println( "n = 100:");
-        time(descending100);
+        averagePrint(arr100);
 	
 
 	System.out.println();
 	
         System.out.println( "n = 1000:");
-        time(descending1000);
+        averagePrint(arr1000);
 	
 
 	System.out.println();
 	
         System.out.println( "n = 10000:");
-        time(descending10000);
+        averagePrint(arr10000);
 	
     }//end main
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Merging
+## Methodology
+We tested our merge sort algorithm on arrays of size 1, 10, 100, 1000, and 10000, and compared the average time that it took each array to be sorted. 
+
+##Findings
+
